e0170545c118818004cb453a420438b60dc864cb
Giles Roadnight <giles@roadnight.name>
2016-12-17 16:23:38 +0000

    Write tests and implement functionality

--- src/lib/queueManager.ts
+++ src/lib/queueManager.ts
@@ -1,23 +1,96 @@
 
-import * as Rx from "rxjs";
+import * as Rx from "rx";
+import {IStreamInfo, StreamCounter} from "stream-item-timer"
 
-export class QueueManager<T> extends Rx.Observable<T>{
-    
-    constructor(source: Rx.Observable<T>){
-        super();
-    }
+export interface IQueueInfo extends IStreamInfo{
+    notStarted: number;
 }
 
-function manageQueue<T>(queueLength: number) {
-   // We *could* do a `var self = this;` here to close over, but see next comment
-   return Rx.Observable.create(subscriber => {
-     // because we're in an arrow function `this` is from the outer scope.
-     var source: Rx.Observable<T> = this;
+export class QueueManager<T> implements IQueueInfo {
 
-     // save our inner subscription
-     var subscription = source.controlled();
+    //  Constructor
 
-     // to return now
-     return subscription;
-   });
+    constructor(source: Rx.Observable<T>, queueLengthProvider: number | Rx.Observable<number>){
+        this._notStartedCounter = new StreamCounter();
+        this._inProgressCounter = new StreamCounter();
+
+        this._controlled = source
+            .do(() => this._notStartedCounter.newItem())
+            .controlled();
+
+        this._downStream = this._controlled
+            .do(() => {
+                this._notStartedCounter.itemComplete();
+                this._inProgressCounter.newItem();
+            })
+            .doOnCompleted(() => this.handleStreamComplete());
+
+        if(typeof queueLengthProvider === "number"){
+            this._queueLength = queueLengthProvider;
+        } else {
+            this._queueLengthDisposable = queueLengthProvider.subscribe(
+                length => this.handleQueueLengthUpdate(length)
+            );
+        }
+
+        this.request();
+    }
+
+    //  Private Variables
+
+    private _queueLengthDisposable: Rx.Disposable;
+
+    private _queueLength: number = 0;
+
+    private _notStartedCounter: StreamCounter;
+    private _inProgressCounter: StreamCounter;
+    private _controlled: Rx.ControlledObservable<T>;
+    private _downStream: Rx.Observable<T>;
+
+    //  Properties
+
+    get queue(): Rx.Observable<T>{
+        return this._downStream;
+    }
+
+    get notStarted(): number{
+        return this._notStartedCounter.inProgress;
+    }
+
+    get inProgress(): number{
+        return this._inProgressCounter.inProgress;
+    }
+
+    get total(): number{
+        return this._notStartedCounter.total;
+    }
+
+    get complete(): number{
+        return this._inProgressCounter.complete;
+    }
+
+    //  Public Functions
+
+    itemComplete(){
+        this._inProgressCounter.itemComplete();
+        this.request();
+    }
+
+    //  Private Functions
+
+    private request(){
+        this._controlled.request(this._queueLength - this._inProgressCounter.inProgress);
+    }
+
+    private handleQueueLengthUpdate(length: number){
+        this._queueLength = length;
+
+        this.request();
+    }
+
+    private handleStreamComplete(){
+        if(this._queueLengthDisposable){
+            this._queueLengthDisposable.dispose();
+        }
+    }
 }
--- src/test/queueManagerTest.ts
+++ src/test/queueManagerTest.ts
@@ -3,8 +3,7 @@ import {QueueManager} from "../lib/queueManager"
 import * as Rx from "rx";
 
 describe("Queue Manager", () => {
-  
-    var itemSource: Rx.Observable<number>;
+
     var queueManager: QueueManager<number>;
 
     var receivedItems: number[];
@@ -13,23 +12,203 @@ describe("Queue Manager", () => {
         receivedItems = [];
     });
 
-    describe("when queue length of 5 is defined at construction", () => {
+    describe("when queue length of 4 is defined at construction", () => {
+
+        describe("and a cold observable used as source", () => {
+
+            beforeEach(() => {
+                const itemSource = Rx.Observable.range(1,10);
+                queueManager = new QueueManager<number>(itemSource,4);
+            });
+
+            it("should release 4 items upon subscription", () => {
+                subscribe();
+
+                expect(receivedItems).toEqual([1,2,3,4]);
+
+                assertCounts(10,6,4,0);
+            });
+
+            it("when item is complete another item should be added to the queue",() => {
+                subscribe();
+
+                assertCountsAsItemsComplete();
+            });
+        });
+    });
+
+    describe("and a hot observable used as source", () => {
+
+        let itemSource: Rx.Subject<number[]>;
 
         beforeEach(() => {
-            itemSource = Rx.Observable.range(0,50);
-            queueManager = new QueueManager<number>(itemSource)
+            itemSource = new Rx.Subject<number[]>();
+            const flattenedSource = itemSource.flatMap(numbers => Rx.Observable.from(numbers))
+            queueManager = new QueueManager<number>(flattenedSource,4);
         });
 
-        it("should release 5 items upon construction", () => {
-            queueManager.subscribe();
+        it("should not release any items upon subscription", () => {
+            subscribe();
 
-            expect(receivedItems).toEqual([1,2,3,4,5]);
+            expect(receivedItems).toEqual([]);
+
+            assertCounts(0,0,0,0);
         });
 
-    })
+        it("when source passes items 4 items added to queue", () => {
+            subscribe();
+
+            itemSource.onNext([1,2,3,4,5,6,7,8,9,10]);
+
+            assertCountsAsItemsComplete();
+        });
+
+        it("after all items complete when more items passed by source 4 more items added to source",() => {
+            subscribe();
+
+            itemSource.onNext([1,2,3,4,5,6,7,8,9,10]);
+
+            assertCountsAsItemsComplete();
+
+            itemSource.onNext([11,12,13,14,15,16,17,18,19,20]);
+
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
+            assertCounts(20,6,4,10);
+        });
+    });
 
     describe("with observable passed to control stream length", () => {
-        
-    })
 
-})
+        let queueLengthSubject: Rx.Subject<number>;
+        let queueLengthSource: Rx.Observable<number>;
+
+        beforeEach(() => {
+            const itemSource = Rx.Observable.range(1,10);
+            queueLengthSubject = new Rx.Subject<number>();
+            queueLengthSource = queueLengthSubject
+                .merge(Rx.Observable.just(1));
+
+            queueManager = new QueueManager<number>(itemSource,queueLengthSource);
+        });
+
+        it("should release one item upon subscription", () => {
+            subscribe();
+
+            expect(receivedItems).toEqual([1]);
+            assertCounts(10,9,1,0);
+            
+            queueLengthSubject.onNext(2);
+            expect(receivedItems).toEqual([1,2]);
+            assertCounts(10,8,2,0);
+            
+            queueLengthSubject.onNext(3);
+            expect(receivedItems).toEqual([1,2,3]);
+            assertCounts(10,7,3,0);
+
+            queueLengthSubject.onNext(1);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3]);
+            assertCounts(10,7,2,1);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3]);
+            assertCounts(10,7,1,2);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4]);
+            assertCounts(10,6,1,3);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5]);
+            assertCounts(10,5,1,4);
+
+            queueLengthSubject.onNext(4);
+
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8]);
+            assertCounts(10,2,4,4);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9]);
+            assertCounts(10,1,4,5);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+            assertCounts(10,0,4,6);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+            assertCounts(10,0,3,7);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+            assertCounts(10,0,2,8);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+            assertCounts(10,0,1,9);
+
+            queueManager.itemComplete();
+            expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+            assertCounts(10,0,0,10);
+        });
+    });
+
+    function assertCountsAsItemsComplete(){
+
+        expect(receivedItems).toEqual([1,2,3,4]);
+        assertCounts(10,6,4,0);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5]);
+        assertCounts(10,5,4,1);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6]);
+        assertCounts(10,4,4,2);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7]);
+        assertCounts(10,3,4,3);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8]);
+        assertCounts(10,2,4,4);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9]);
+        assertCounts(10,1,4,5);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+        assertCounts(10,0,4,6);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+        assertCounts(10,0,3,7);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+        assertCounts(10,0,2,8);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+        assertCounts(10,0,1,9);
+
+        queueManager.itemComplete();
+        expect(receivedItems).toEqual([1,2,3,4,5,6,7,8,9,10]);
+        assertCounts(10,0,0,10);
+    }
+
+    function assertCounts(total: number, notStarted: number, inProgress: number, complete: number){
+        expect(queueManager.total).toEqual(total);
+        expect(queueManager.notStarted).toEqual(notStarted);
+        expect(queueManager.inProgress).toEqual(inProgress);
+        expect(queueManager.complete).toEqual(complete);
+    };
+
+    function subscribe(){
+        queueManager.queue.subscribe(item => receivedItems.push(item));
+    };
+
+});
